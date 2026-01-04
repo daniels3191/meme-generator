716ce8a48abd59fa74da3960ea266b250a071fb2
daniels3191 <71701543+daniels3191@users.noreply.github.com>
2026-01-04 21:42:03 +0200

    First architecture and layout

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 'use strict'
+
+let gElCanvas
+let gCtx
+
+function onInit() {
+    gElCanvas = document.querySelector('canvas')
+    gCtx = gElCanvas.getContext('2d')
+
+    resizeCanvas()
+
+    window.addEventListener('resize', () => resizeCanvas())
+}
+
+function renderMeme(elImg) {
+    console.log(elImg);
+
+
+}
+
+function resizeCanvas() {
+    const elContainer = document.querySelector('.canvas-container')
+
+    gElCanvas.width = elContainer.clientWidth
+
+
+}
+
+function renderMeme(elImg) {
+    // const elImg = new Image()
+    // elImg.src = 'img/gallery/1.jpg'
+    console.log(elImg);
+    
+
+    elImg.onload = () =>
+        gCtx.drawImage(elImg, 0, 0, elImg.naturalWidth, elImg.naturalHeight)
+}
